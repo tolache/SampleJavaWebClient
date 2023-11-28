af9c955a3f810ba7ff9116191ca9a43132ff2029
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2023-11-28 09:56:19 +0100

    parametrize buffer size

--- SampleJavaWebClient.java
+++ SampleJavaWebClient.java
@@ -1,23 +1,27 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+
+import static java.lang.Integer.parseInt;
 
 public class SampleJavaWebClient {
 
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java SampleJavaWebClient <\"Header1: Value1[, Header2: Value2]\"> <DownloadURL> <OutputFile>");
+        if (args.length != 4) {
+            System.out.println("Usage: java SampleJavaWebClient <\"Header1: Value1[, Header2: Value2]\"> <DownloadURL> <OutputFile> <BufferSizeBytes>");
             System.exit(1);
         }
 
         String headers = args[0];
         String downloadUrl = args[1];
         String outputFile = args[2];
+        int bufferSizeBytes = parseInt(args[3]);
 
         long startTime = System.currentTimeMillis();
 
@@ -36,7 +40,7 @@ public class SampleJavaWebClient {
                  FileOutputStream outputStream = new FileOutputStream(outputFile)) {
 
                 // Download the file
-                byte[] buffer = new byte[65536];
+                byte[] buffer = new byte[bufferSizeBytes];
                 int bytesRead;
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, bytesRead);
@@ -49,9 +53,10 @@ public class SampleJavaWebClient {
             long endTime = System.currentTimeMillis();
             double fileSizeInBytes = Files.size(Path.of(outputFile));
             double fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-            double totalTimeInSeconds = (endTime - startTime) / 1000.0;
-            double averageSpeedInMegabytesPerSecond = fileSizeInMegabytes / totalTimeInSeconds;
+            double totalTimeInSeconds = round((endTime - startTime) / 1000.0, 2);
+            double averageSpeedInMegabytesPerSecond = round(fileSizeInMegabytes / totalTimeInSeconds, 2);
 
+            System.out.println("Buffer size: " + bufferSizeBytes / 1024 + " KB");
             System.out.println("File size: " + fileSizeInMegabytes + " MB");
             System.out.println("Total time: " + totalTimeInSeconds + " seconds");
             System.out.println("Average download speed: " + averageSpeedInMegabytesPerSecond + " MB/s");
@@ -59,5 +64,13 @@ public class SampleJavaWebClient {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
